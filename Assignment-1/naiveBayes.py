76a3fed4b9add53dc7566d69f888f3a6d4e3ffb0
Chris Holland <christopherholland@uvic.ca>
2019-09-24 22:46:25 -0700

    started naiveBayes.py

--- Assignment-1/naiveBayes.py
+++ Assignment-1/naiveBayes.py
@@ -1,1 +1,9 @@
-print("Hello, world!")
+'''
+Chris Holland
+naiveBayes.py
+
+Naive Bayes Text Classification algorithm.
+Based off the pseudocode provided in class.
+'''
+
+def trainMultinomial(line, document):
